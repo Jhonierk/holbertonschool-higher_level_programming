6cf8a4412ebf2dfc7dd489645ec3d7ae2d6f299d
Jhonnyer Otalvaro <64118164+Jhonierk@users.noreply.github.com>
2020-09-01 14:15:09 -0500

    0x01-0 Positive anything is better than negative nothing

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-print()
 if number > 0 : 
     print("{} is positive".format(number))
 elif number < 0:
     print("{} is negative".format(number))
-elif number == 0:
+else:
     print("{} is zero".format(number))
