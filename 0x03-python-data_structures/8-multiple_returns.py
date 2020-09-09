65e8a6c5db4592e56cd0ee32529df074d3133322
Jhonnyer Otalvaro <64118164+Jhonierk@users.noreply.github.com>
2020-09-09 17:00:42 -0500

    fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if not sentence:
-        return (len(sentence), None)
+    if len(sentence) > 0:
+        return tuple([len(sentence), sentence[0]])
     else:
-        return (len(sentence), sentence[0])
+        return tuple([0, None])
