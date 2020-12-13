e79f5914537093d4a150067e1ce0abd7db39a229
Jhonnyer Otalvaro <64118164+Jhonierk@users.noreply.github.com>
2020-12-13 09:57:45 -0500

    try fix

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
+""" script that use a ForeignKey
 """
-Write a Python file similar to model_state.py named model_city.py
-that contains the class definition of a City.
-"""
+
 from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State, Base
+from model_state import Base, State
 from model_city import City
 
+if __name__ == "__main__":
 
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(argv[1], argv[2], argv[3]))
-
+    user = argv[1]
+    passw = argv[2]
+    nm_db = argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(user, passw, nm_db))
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    for city, state in session\
-        .query(City, State)\
-        .filter(City.state_id == State.id)\
-            .order_by(City.id):
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    join = session.query(City, State).filter(State.id == City.state_id).all()
+    for ct, st in join:
+        print("{}: ({}) {}" .format(st.name, ct.id, ct.name))
+    session.close()
