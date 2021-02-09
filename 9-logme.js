51e9f9099cae4d8ce50849063b45037dd4d75c3c
Jhonnyer Otalvaro <64118164+Jhonierk@users.noreply.github.com>
2021-02-08 20:06:42 -0500

    Delete 9-logme.js

--- 9-logme.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-/*  function that prints the number of arguments already printed and the new argument value */
-let num = 0;
-exports.logMe = function (item) {
-  console.log(num + ': ' + item);
-  num++;
-};
