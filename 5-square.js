96046adf605c917baf9057d87033160c51e886e6
Jhonnyer Otalvaro <64118164+Jhonierk@users.noreply.github.com>
2021-02-08 20:06:04 -0500

    Delete 5-square.js

--- 5-square.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-/*  class Square that defines a square and inherits from Rectangle of 4-rectangle.js */
-class Square extends require('./4-rectangle') {
-  constructor (size) {
-    super(size, size);
-  }
-}
-module.exports = Square;
